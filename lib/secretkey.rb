66b891d24155d8f1898f7c896436041bfc57331b
Dinusha Bodhinayake <dinushab@jobready.com.au>
2014-07-25 09:56:08 +1000

    [SHARED-15] updated readme [SHARED-15] modified timestamp method [SHARED-15] added invalid timestamp spec [SHARED-15] modified gemfile [SHARED-15] modified secretkey gem so that it initializes and produces timestamp on its on

--- lib/secretkey.rb
+++ lib/secretkey.rb
@@ -2,9 +2,28 @@ require 'digest'
 
 module SecretKey
   class SecretKey
-    def self.generate_token(key, secret, timestamp)
-      raise ArgumentError, 'Time Stamp is not a valid integer.' unless timestamp.is_a? Integer
-      Digest::SHA1.hexdigest("#{key}:#{secret}:#{timestamp}")
+
+    def initialize(key, secret, options={})
+      @key = key
+      @secret = secret
+      @options = options
+
+      extract_options!
+    end
+
+    def token
+      raise ArgumentError, 'Time Stamp is not a valid integer.' unless @timestamp.is_a? Integer
+      Digest::SHA1.hexdigest("#{@key}:#{@secret}:#{@timestamp}")
+    end
+
+    def timestamp
+      @timestamp ||= Time.now.to_i
+    end
+
+    private
+
+    def extract_options!
+      @timestamp = @options.fetch(:timestamp, timestamp)
     end
   end
 end
