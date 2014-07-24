2d94f80ba2b54e548d14ee66a829607ebf233971
Dinusha Bodhinayake <dinushab@jobready.com.au>
2014-07-24 17:04:02 +1000

    [SHARED-15] modified secretkey gem so that it initializes and produces timestamp on its on

--- lib/secretkey.rb
+++ lib/secretkey.rb
@@ -2,9 +2,31 @@ require 'digest'
 
 module SecretKey
   class SecretKey
-    def self.generate_token(key, secret, timestamp)
-      raise ArgumentError, 'Time Stamp is not a valid integer.' unless timestamp.is_a? Integer
-      Digest::SHA1.hexdigest("#{key}:#{secret}:#{timestamp}")
+
+    attr_accessor :timestamp
+
+    def initialize(key, secret, options={})
+      @key = key
+      @secret = secret
+      @options = options
+
+      extract_options!
     end
+
+    def token
+      raise ArgumentError, 'Time Stamp is not a valid integer.' unless @timestamp.is_a? Integer
+      Digest::SHA1.hexdigest("#{@key}:#{@secret}:#{@timestamp}")
+    end
+
+    private
+
+    def extract_options!
+      @timestamp = @options.fetch(:timestamp, default_timestamp)
+    end
+
+    def default_timestamp
+      @default_timestamp ||= Time.now.to_i
+    end
+
   end
 end
