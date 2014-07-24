702ad6ab54461eb07f309083a1f7ba5706ddbb2b
Dinusha Bodhinayake <dinushab@jobready.com.au>
2014-07-25 08:44:05 +1000

    [SHARED-15] added invalid timestamp spec

--- spec/unit/secretkey_spec.rb
+++ spec/unit/secretkey_spec.rb
@@ -16,14 +16,27 @@ describe SecretKey::SecretKey do
     end
 
     context 'when timestamp is provided' do
-      let(:timestamp) { Time.new(2014, 07, 24, 16, 54, 00).to_i }
       let(:secret_key) { described_class.new(key, secret, timestamp: timestamp) }
-      let(:expected_token) { Digest::SHA1.hexdigest("#{key}:#{secret}:#{timestamp}") }
 
-      it 'produces a valid token' do
-        expect(secret_key.token).to eq(expected_token)
+      context 'when timestamp provided is valid' do
+        let(:timestamp) { Time.new(2014, 07, 24, 16, 54, 00).to_i }
+        let(:expected_token) { Digest::SHA1.hexdigest("#{key}:#{secret}:#{timestamp}") }
+
+        it 'produces a valid token' do
+          expect(secret_key.token).to eq(expected_token)
+        end
+      end
+
+      context 'when timestamp is invalid' do
+        let(:timestamp) { 'askdfjlakshfdkjasd' }
+
+        it 'produces an Argument Error' do
+          expect(lambda { secret_key.token }).to raise_error(ArgumentError, 'Time Stamp is not a valid integer.')
+        end
       end
     end
+
+
   end
 
 end
