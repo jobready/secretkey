9191cba4617ebd73dce0935129e7e4dcd6a733d9
Dinusha Bodhinayake <dinushab@jobready.com.au>
2014-07-25 09:58:39 +1000

    [SHARED-15] updated readme [SHARED-15] modified timestamp method [SHARED-15] added invalid timestamp spec [SHARED-15] modified gemfile [SHARED-15] modified secretkey gem so that it initializes and produces timestamp on its on
    [SHARED-14] modified timestamp variable to be more consistent
    [SHARED-14] added travis yaml
    [SHARED-14] modified version control
    [SHARED-14] changed timestamp parameter to epoch based timestamp
    [SHARED-14] updated Readme
    [SHARED-14] added generate token method
    [SHARED-14] got basic functionality passing
    [SHARED-14] updated files

--- spec/unit/secretkey_spec.rb
+++ spec/unit/secretkey_spec.rb
@@ -4,23 +4,36 @@ describe SecretKey::SecretKey do
 
   let!(:key) { Faker::Number.number(15) }
   let!(:secret) { Faker::Bitcoin.address }
-  let!(:test_timestamp) { Time.new(2014, 07, 24, 12, 53).to_i }
-  let!(:generated_token) { Digest::SHA1.hexdigest("#{key}:#{secret}:#{test_timestamp}") }
 
-  describe '.generate_token' do
-    context 'when timestamp is valid' do
-      let!(:timestamp) { Time.new(2014, 07, 24, 12, 53).to_i }
-      it 'produces a token' do
-        expect(described_class.generate_token(key, secret, timestamp)).to eq(generated_token)
+  describe '#token' do
+    context 'when timestamp is not provided' do
+      let(:secret_key) { described_class.new(key, secret) }
+      let(:timestamp) { secret_key.timestamp }
+      let(:expected_token) { Digest::SHA1.hexdigest("#{key}:#{secret}:#{timestamp}") }
+      it 'produces a valid token' do
+        expect(secret_key.token).to eq(expected_token)
       end
     end
 
-    context 'when timestamp is invalid' do
-      let!(:timestamp) { 'sometext' }
-      it 'raises an invalid timestamp error' do
-        expect(lambda { described_class.generate_token(key, secret, timestamp) }).to raise_error(ArgumentError, 'Time Stamp is not a valid integer.')
+    context 'when timestamp is provided' do
+      let(:secret_key) { described_class.new(key, secret, timestamp: timestamp) }
+
+      context 'when timestamp provided is valid' do
+        let(:timestamp) { Time.new(2014, 07, 24, 16, 54, 00).to_i }
+        let(:expected_token) { Digest::SHA1.hexdigest("#{key}:#{secret}:#{timestamp}") }
+
+        it 'produces a valid token' do
+          expect(secret_key.token).to eq(expected_token)
+        end
+      end
+
+      context 'when timestamp is invalid' do
+        let(:timestamp) { 'askdfjlakshfdkjasd' }
+
+        it 'produces an Argument Error' do
+          expect(lambda { secret_key.token }).to raise_error(ArgumentError, 'Time Stamp is not a valid integer.')
+        end
       end
     end
   end
-
 end
